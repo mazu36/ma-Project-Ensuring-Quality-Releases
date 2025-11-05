4eae9ec85e7a4c076107f4acfda6b47a9dea3e60
mazu36 <65269375+mazu36@users.noreply.github.com>
2025-11-05 15:18:28 +0100

    selenium

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -15,11 +15,8 @@ def login (user, password):
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
     options.add_argument("--headless") 
-
-    # MDE
-    #options.add_argument("--headless")
     options.add_argument("--no-sandbox")
-    #options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-dev-shm-usage")
     #options.add_argument("--disable-gpu")
     #options.add_argument("--remote-debugging-port=9222")
     
@@ -110,5 +107,6 @@ print(f"Final number of items in the cart after removing all products: {cart_ite
 time.sleep(5)  # Wait for 5 seconds
 
 driver.quit()
+
 
 
