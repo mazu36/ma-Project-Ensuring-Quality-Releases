9b8dbbfe4554b3f5f46d3dd9bb3b997bb88b0377
mazu36 <65269375+mazu36@users.noreply.github.com>
2025-11-05 14:28:52 +0100

    undo some selenium options

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -19,9 +19,9 @@ def login (user, password):
     # MDE
     #options.add_argument("--headless")
     options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--remote-debugging-port=9222")
+    #options.add_argument("--disable-dev-shm-usage")
+    #options.add_argument("--disable-gpu")
+    #options.add_argument("--remote-debugging-port=9222")
     
     driver = webdriver.Chrome(options=options)
 
@@ -110,4 +110,5 @@ print(f"Final number of items in the cart after removing all products: {cart_ite
 time.sleep(5)  # Wait for 5 seconds
 
 driver.quit()
+
 
